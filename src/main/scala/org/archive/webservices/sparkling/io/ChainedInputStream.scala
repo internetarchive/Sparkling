757795ccc981c8dfa874dcbd2b7e61e3bb24705f
Helge Holzmann <helge@archive.org>
2024-02-02 14:58:03 +0100

    refactor ChainedInputStream

--- src/main/scala/org/archive/webservices/sparkling/io/ChainedInputStream.scala
+++ src/main/scala/org/archive/webservices/sparkling/io/ChainedInputStream.scala
@@ -7,7 +7,7 @@ import scala.util.Try
 class ChainedInputStream(in: Iterator[InputStream], nextOnError: Boolean = false) extends InputStream {
   val buffered: BufferedIterator[InputStream] = in.buffered
 
-  @tailrec private def next(eof: Int, action: InputStream => Int): Int = if (buffered.hasNext) {
+  @tailrec private def next[A](eof: A, action: InputStream => A): A = if (buffered.hasNext) {
     val r = if (nextOnError) Try(action(buffered.head)).getOrElse(eof) else action(buffered.head)
     if (r == eof) {
       Try(buffered.head.close())
@@ -16,19 +16,10 @@ class ChainedInputStream(in: Iterator[InputStream], nextOnError: Boolean = false
     } else r
   } else eof
 
-  @tailrec private def nextLong(eof: Long, action: InputStream => Long): Long = if (buffered.hasNext) {
-    val r = if (nextOnError) Try(action(buffered.head)).getOrElse(eof) else action(buffered.head)
-    if (r == eof) {
-      Try(buffered.head.close())
-      buffered.next()
-      nextLong(eof, action)
-    } else r
-  } else eof
-
   override def read(): Int = next(-1, _.read())
   override def read(b: Array[Byte]): Int = next(-1, _.read(b))
   override def read(b: Array[Byte], off: Int, len: Int): Int = next(-1, _.read(b, off, len))
-  override def skip(n: Long): Long = nextLong(0, _.skip(n))
+  override def skip(n: Long): Long = next(0, _.skip(n))
   override def available(): Int = next(0, _.available)
   override def close(): Unit = for (s <- buffered) s.close()
   override def markSupported(): Boolean = false
